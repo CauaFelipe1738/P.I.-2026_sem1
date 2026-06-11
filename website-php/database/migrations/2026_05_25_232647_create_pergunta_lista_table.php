49940d5fa5e0cfe9d818c58b11beec03a1f3da69
kevin3068 <268755134+kevin3068@users.noreply.github.com>
2026-06-10 21:24:43 -0300

    Update Drop Migrations

--- website-php/database/migrations/2026_05_25_232647_create_pergunta_lista_table.php
+++ website-php/database/migrations/2026_05_25_232647_create_pergunta_lista_table.php
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('pergunta_listas');
+        Schema::dropIfExists('pergunta_lista');
     }
 };
--- website-php/database/migrations/2026_05_25_233015_create_funcionario_pergunta_lista_table.php
+++ website-php/database/migrations/2026_05_25_233015_create_funcionario_pergunta_lista_table.php
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('funcionario_pergunta_listas');
+        Schema::dropIfExists('funcionario_pergunta_lista');
     }
 };
